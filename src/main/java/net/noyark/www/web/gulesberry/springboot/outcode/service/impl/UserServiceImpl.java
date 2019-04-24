def2222901107e728c1a80bd3cfe678a20cd0005
KevinKVJ <jiakaiwen5216@gmail.com>
2019-04-24 17:29:05 +0800

    --修改了注册逻辑(已完成),未完成控制器

--- src/main/java/net/noyark/www/web/gulesberry/springboot/outcode/service/impl/UserServiceImpl.java
+++ src/main/java/net/noyark/www/web/gulesberry/springboot/outcode/service/impl/UserServiceImpl.java
@@ -19,47 +19,50 @@ public class UserServiceImpl implements UserService
     UserMapper userMapper;
 
     @Override
-    public void signup(User user) throws DuplicatedUsernameException, InsertException
-    {
+    public void signup(User user) throws DuplicatedUsernameException, InsertException {
         User result = userMapper.findByUsername(user.getUsername());
-        if(result!=null&&result.getIsDelete()==0)
+        if (result == null||result.getIsDelete()==1)
         {
+            //密码加密
+            String salt = UUID.randomUUID().toString();
+            System.out.println(salt);
+            System.out.println(salt);
+            user.setSalt(salt);
+            user.setIsDelete(0);
+            //执行加密
+            String md5Password = getMd5Password(user.getPassword(), salt);
+            System.out.println(md5Password);
+            user.setPassword(md5Password);
+
+            user.setCreatedUser(user.getUsername());
+            user.setCreatedTime(new Date());
+            user.setModifiedTime(new Date());
+            user.setModifiedUser(user.getUsername());
+
+            Integer row = userMapper.insertUser(user);
+            System.err.println(row);
+
+            if (row != 1) {
+                throw new InsertException("注册发生未知错误:数据库插入异常,请联系管理员");
+            }
+        } else {
             throw new DuplicatedUsernameException("用户名已存在,请重新输入");
         }
 
-        //密码加密
-        String salt = UUID.randomUUID().toString();
-        System.out.println(salt);
-        System.out.println(salt);
-        user.setSalt(salt);
-
-        //执行加密
-        String md5Password = getMd5Password(user.getPassword(),salt);
-        System.out.println(md5Password);
-        user.setPassword(md5Password);
-
-        user.setCreatedUser(user.getUsername());
-        user.setCreatedTime(new Date());
-        user.setModifiedTime(new Date());
-        user.setModifiedUser(user.getUsername());
-
-        Integer row = userMapper.insertUser(user);
-        System.err.println(row);
-
-        if(row!=1){
-            throw new InsertException("注册发生未知错误:数据库插入异常,请联系管理员");
-        }
     }
 
-    private String getMd5Password(String password,String salt)
-    {
-        String str = salt + password + salt;
-
-        for (int i = 0;i < 5;i++)
+        private String getMd5Password(String password,String salt)
         {
-            str = DigestUtils.md5DigestAsHex(str.getBytes()).toString();
+            String str = salt + password + salt;
+
+            for (int i = 0;i < 5;i++)
+            {
+                str = DigestUtils.md5DigestAsHex(str.getBytes()).toString();
+            }
+
+            return str;
         }
 
-        return str;
-    }
+
+
 }
--- src/test/java/net/noyark/www/web/gulesberry/springboot/outcode/UserServiceTestCase.java
+++ src/test/java/net/noyark/www/web/gulesberry/springboot/outcode/UserServiceTestCase.java
@@ -20,7 +20,7 @@ public class UserServiceTestCase {
     @Test
     public void signup() {
         User user = new User();
-        user.setUsername("fff");
+        user.setUsername("EEE");
         user.setPassword("123");
         user.setNickname("nickname123");
         user.setAddress("luluRoad");
