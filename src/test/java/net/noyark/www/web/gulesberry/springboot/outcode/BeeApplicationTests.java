fc871e03f397b93820bfc5f062a5b3b46be229e7
MagicLu550 <843983738@qq.com>
2019-04-24 10:08:12 +0800

    更改了包的主运行类结构

--- src/test/java/net/noyark/www/web/gulesberry/springboot/outcode/BeeApplicationTests.java
+++ src/test/java/net/noyark/www/web/gulesberry/springboot/outcode/BeeApplicationTests.java
@@ -3,10 +3,12 @@ package net.noyark.www.web.gulesberry.springboot.outcode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration
 public class BeeApplicationTests {
 
     @Test
